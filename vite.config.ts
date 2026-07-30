bd1be7ed557d47d9e35bdd84f7fe5dc3124b5206
Purushothamansuba <purusothaman491@gmail.com>
2026-07-30 22:16:23 +0530

    Configure Nitro for Vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,16 @@
 import { defineConfig } from "vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import { nitro } from "nitro/vite";
 import viteReact from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [tailwindcss(), tanstackStart(), viteReact()],
-  resolve: {
-    tsconfigPaths: true,
-  },
+  plugins: [
+    tailwindcss(),
+    tanstackStart(),
+    nitro({
+      preset: "vercel",
+    }),
+    viteReact(),
+  ],
 });
